22fe26a8f7e193b5d0514f90cb071f54e393bc4a
Damon Nyhan <damon.nyhan@gmail.com>
2019-03-20 06:59:12 -0700

    removed

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 
 func main() {
 	config := pubnub.NewConfig()
-	config.SubscribeKey = "sub-c-6548f134-1950-11e9-9cda-0ee81137d4bc"
-	config.PublishKey = "pub-c-56dc14e3-fc0c-44f6-8fe0-9c0989edbebb"
+	config.SubscribeKey = "demo"
+	config.PublishKey = "demo"
 	pn := pubnub.NewPubNub(config)
 	fmt.Println("Welcome to Space Race!")
 	newLobby("", "", pn) //This creates the lobby for a new game
